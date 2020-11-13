4cf0e02a60e18aac9c1b046044c681d5cf71533f
momokaaraki <68629509+momokaaraki@users.noreply.github.com>
2020-11-13 12:02:58 +0900

    hennkou

--- sample/js/script04.js
+++ sample/js/script04.js
@@ -18,14 +18,7 @@ $(function(){
     separate = documentH / 3;//内容の高さを3分割した数値
 
     //３分割した数値とどれだけスクロールしたかを比較→bodyにクラスをつける。あとはCSSファイルでデザイン変更
-    if(scrollTop < separate){
-      $("body").removeClass().addClass("one")
-    }else if(scrollTop < separate * 2){
-      $("body").removeClass().addClass("two")
-    }else{
-      $("body").removeClass().addClass("three")
-    }
-
+    
     console.log(windowH,documentH,scrollTop)
 
     $("#wave").css("background-position-x",scrollTop/10)//右上の背景画像の位置変更(CSS)
